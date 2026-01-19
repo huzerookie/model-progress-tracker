7c5c3c190c962d8ce8b8725e4493ec15f3fa4b24
Huzefa <ultimatehuzefa@gmail.com>
2026-01-20 03:59:55 +0530

    Removing WebConfig from registering in spring directory

--- src/main/java/com/expressivemodel/modelprogresstracker/security/WebConfig.java
+++ src/main/java/com/expressivemodel/modelprogresstracker/security/WebConfig.java
@@ -1,10 +1,9 @@
 package com.expressivemodel.modelprogresstracker.security;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
