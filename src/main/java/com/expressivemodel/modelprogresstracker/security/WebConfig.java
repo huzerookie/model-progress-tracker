74a7358049d44339e913944bc4ac7766a959e3a5
Huzefa <ultimatehuzefa@gmail.com>
2026-01-20 02:10:04 +0530

    Adding render url in cors origin

--- src/main/java/com/expressivemodel/modelprogresstracker/security/WebConfig.java
+++ src/main/java/com/expressivemodel/modelprogresstracker/security/WebConfig.java
@@ -1,17 +1,19 @@
 package com.expressivemodel.modelprogresstracker.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:5500","http://127.0.0.1:5500") 
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true);
-    }
+
+	@Value("${render.ui.url}")
+	private String renderURL;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**").allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500", renderURL)
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true);
+	}
 }
