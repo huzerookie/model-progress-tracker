baf475fb77b673d499797f7912d4371f976e8f7b
Huzefa <ultimatehuzefa@gmail.com>
2026-01-20 01:32:09 +0530

    Minor Changes for host origins allowed

--- src/main/java/com/expressivemodel/modelprogresstracker/security/SecurityConfig.java
+++ src/main/java/com/expressivemodel/modelprogresstracker/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.expressivemodel.modelprogresstracker.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -42,7 +44,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*"); // 👈 frontend origin
+        config.setAllowedOriginPatterns(List.of("*")); // ✅ use patterns instead of addAllowedOrigin("*")
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
